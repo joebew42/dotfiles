35cf0e8cde7c8450582cf72dce2b0928c83d4a08
JoeBew42 <joebew42@gmail.com>
2017-09-24 11:59:32 +0200

    just extract the _pwd

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,31 +1,36 @@
 function fish_prompt
   set laststatus $status
+  function _pwd
+    set realhome ~
+    if [ $realhome = $PWD ]
+      echo -n (set_color green)\u2302' '
+    else
+      echo -n (basename $PWD)' '
+    end
+  end
   function _git_branch_name
-    echo -n  (set_color yellow)(git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+    echo -n (set_color yellow)(git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
   end
   function _git_is_dirty
-    echo (git status -s --ignore-submodules=dirty ^/dev/null)
+    echo -n (git status -s --ignore-submodules=dirty ^/dev/null)
   end
   function _git_status
     if [ (_git_is_dirty) ]
-      set git_status (set_color red) \u2717
+      echo -n (set_color red)\u2717
     else
-      set git_status (set_color green) \u2713
+      echo -n (set_color green)\u2713
     end
-    printf '%s' $git_status
   end
   function _git_info
     if [ (_git_branch_name) ]
-      printf '%s %s ' (_git_branch_name) (_git_status)
-    else
-      printf '%s ' (prompt_pwd)
+      echo -n "$git_info"(_git_branch_name)' '(_git_status)' '
     end
   end
   function _prompt_start
-    printf (set_color blue)'❯ '
+    echo -n (set_color blue)'❯ '
   end
   function _prompt_end
-    printf (set_color magenta)'❯ '
+    echo -n (set_color magenta)'❯ '
   end
 
   test $SSH_TTY
@@ -35,5 +40,5 @@ function fish_prompt
   and echo (set_color red)"#"
 
   # Main
-  printf '%s%s%s' (_prompt_start) (_git_info) (_prompt_end)
+  printf '%s%s%s%s' (_prompt_start) (_pwd) (_git_info) (_prompt_end)
 end
