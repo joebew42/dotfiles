95d9bced6af3bc7556ac04ea25c7248ff9fc50f9
Joe Bew <joebew42@gmail.com>
2020-11-21 11:44:53 +0100

    Add watch.sh: A utility to run command when files changes

---
+++ bin/watch.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+[ $# -eq 0 ] && { echo "Usage: $0 directory-to-watch command-to-run"; exit 1; }
+
+folder_to_watch=$1; shift
+
+if [ ! -d $folder_to_watch ]
+then
+  echo "The folder ${folder_to_watch} does not exists.";
+  exit 1
+fi
+
+command_to_run=$@
+
+hidden_files='^\./\.'
+
+inotifywait -e create -e delete -e modify \
+            -e moved_from -e moved_to -e attrib \
+            --exclude $hidden_files \
+            -m $folder_to_watch -r -q |
+  while read path action filename; do
+    echo "${action} triggered on: ${path}${filename}, running ${command_to_run} ..."
+    eval $command_to_run
+  done
