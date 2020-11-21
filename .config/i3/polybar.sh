9a5db38f98e190c5d0c81d5e360259adc8fc80bf
Joe Bew <joebew42@gmail.com>
2020-11-21 13:58:00 +0100

    Configure polybar

--- .config/i3/polybar.sh
+++ .config/i3/polybar.sh
@@ -10,6 +10,6 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-for m in $(polybar --list-monitors | grep " connected" | cut -d":" -f1); do
+for m in $(polybar --list-monitors | cut -d":" -f1); do
   MONITOR=$m polybar --reload example &
 done
